9a6265f4f8ea2abd83421ac8b4b909d912b71166
Việt Hoàng <90245052+viethoang2810@users.noreply.github.com>
2023-06-30 10:57:50 +0700

    Update data for seeding

--- BusinessObjects/Migrations/20230628193408_SeedingData.cs
+++ BusinessObjects/Migrations/20230628193408_SeedingData.cs
@@ -8,67 +8,68 @@ namespace BusinessObjects.Migrations
         {
             //Seed data to database
             migrationBuilder.InsertData(
-                table: "User",
-                columns: new[] { "FullName", "BranchName", "UserName", "Password", "Address", "RoleID", "Email", "IsDeleted" },
+                table: "Users",
+                columns: new[] { "Id", "Fullname", "BrandName", "UserName", "Password", "Address", "RoleId", "Email", "IsDeleted" , "EmailConfirmed", "PhoneNumberConfirmed", "" +
+                "TwoFactorEnabled", "LockoutEnabled", "AccessFailedCount" },
                 values: new object[,]
                 {
-                    {"doviethoang","dirtycoin","hoangdv6","abc","District 9, Ho Chi Minh",2,"hoangdv6@gmail.com",1 },
-                    {"lexuandai","nike","hoangdv6","dailx","Nha Be , Ho Chi Minh",2,"dailx@gmail.com",1 },
-                    {"huuvinh","adidas","vinhn","vinh","Binh Thanh District, Ho Chi Minh",2,"vinhhn@gmail.com",1 },
-                    {"thinhphu","TeeLab","thinh","thinhp","District 9, Ho Chi Minh",2,"hoangdv6@gmail.com",1 },
-                    {"oanhvtk","Adidas","oanhvtk","oanh","Tan Binh, Ho Chi Minh",2,"oanhvtk@gmail.com",1 },
+                    {"US001","doviethoang","dirtycoin","hoangdv6","abc","District 9, Ho Chi Minh",1,"hoangdv6@gmail.com",false,true,false,false,false,0},
+                    {"US002","lexuandai","nike","hoangdv6","dailx","Nha Be , Ho Chi Minh",2,"dailx@gmail.com",false,true,false,false,false,0 },
+                    {"US003","huuvinh","adidas","vinhn","vinh","Binh Thanh District, Ho Chi Minh",2,"vinhhn@gmail.com",false,true,false,false,false,0 },
+                    {"US004","thinhphu","TeeLab","thinh","thinhp","District 9, Ho Chi Minh",2,"hoangdv6@gmail.com",false,false,false,false,false,0 },
+                    {"US005","oanhvtk","Adidas","oanhvtk","oanh","Tan Binh, Ho Chi Minh",2,"oanhvtk@gmail.com",false,true,false,false,false,0 },
 
                 }
                 );
 
             migrationBuilder.InsertData(
                 table: "Categories",
-                columns: new[] { "CategoryName", "UserID", "IsDeleted" },
+                columns: new[] {"CategoryName", "UserId", "IsDeleted" },
                 values: new object[,]
                 {
-                    {"Short Pan" , 1 , 1},
-                    {"Shoes" , 2,1},
-                    {"Short",3,1 },
-                    {"Shirt",3 , 0},
-                    {"Jacket",2,0},
-                    {"Long pan" , 1 , 0 }
+                    {"Short Pan" ,"US001" , false},
+                    {"Shoes" ,"US002",false},
+                    {"Short","US003",false },
+                    {"Shirt","US003" , true},
+                    {"Jacket","US002",true},
+                    {"Long pan" , "US001" , false }
                 }
                 );
             migrationBuilder.InsertData(
                 table: "Vouchers",
-                columns: new[] { "VoucherName", "Code", "Value", "ExpinationDate", "Quantity", "UserId", "IsDeleted" },
+                columns: new[] { "VoucherName", "Code", "Value", "CreationDate","ExpirationDate", "Quantity", "UserId", "IsDeleted" },
                 values: new object[,]
                 {
-                    {"BacktoSchool","BTS001",30,"2023-09-30",10,2,1},
-                    {"BlackFriday","BF001",60,"2023-11-24",2,4,1},
-                    {"BigSale","BS001",35,"2023-08-20",3,3,1},
-                    {"WomanDay","WD001",25,"2023-03-09",2,5,1},
+                    {"BacktoSchool","BTS001",30,"2023-09-30","2023-09-30",10,"US002",false},
+                    {"BlackFriday","BF001",60,"2023-11-24","2023-11-24",2,"US004",false},
+                    {"BigSale","BS001",35,"2023-06-30","2023-08-20",3,"US003",false},
+                    {"WomanDay","WD001",25,"2023-06-30","2023-03-09",2,"US005",false},
                 }
                 );
 
             migrationBuilder.InsertData(
-                table: "Product",
-                columns: new[] { "CategoryId", "UserId", "ProductName", "Description", "ImageUrl", "Price", "CreationDate", "IsDeleted" },
+                table: "Products",
+                columns: new[] {"CategoryId", "UserId", "ProductName", "Description", " ImageUrl", "Price", "CreationDate", "IsDeleted" },
                 values: new object[,]
                 {
-                    {1,1,"Short Pan" ,"Short Pan from Dirty Coins" ,"https://bizweb.dktcdn.net/100/369/010/products/1-e95372e6-c524-4715-a07d-791c91ebe7ea.jpg?v=1656325763640",300000,"GETDATE()",1 },
-                    {2,2,"Nike AF1 Mid" ,"Short Pan from Dirty Coins" ,"https://cdn.vortexs.io/api/images/372b5357-2d66-4d6d-8b89-9bed5ea69bca/1920/w/50-giay-nike-air-force-1-mid-all-white-o-314195-113.jpeg",1600000,"GETDATE()",1 },
-                    {3,4,"Short TeeLab" ,"Short Local Band Unisex Special" ,"https://bizweb.dktcdn.net/thumb/large/100/415/697/products/ta9216-11k7zwk1-1-7fw9-hinh-mat-sau-0-e7d37221-9eeb-4978-868f-9193908bee74.jpg?v=1685936039000",190000,"GETDATE()",1 },
-                    {2,3,"Adidas Ultra Boots" ,"Adidas Ultra Boots Black-White" ,"https://giayxshop.vn/wp-content/uploads/2019/01/z3651678985400_f5c2a3afb17825c7244e3a4698bb798c-scaled.jpg",2500000,"GETDATE()",1 },
-                    {5,5,"Adidas Jacket" ,"" ,"https://cdn.vortexs.io/api/images/a69812de-f2c5-4243-abe0-72cf736ab4d7/1920/w/ao-khoac-jacket-adidas-firebird-track-red-hot-gf0211.jpeg",300000,"GETDATE()",1 },
+                    {43,"US001","Short Pan" ,"Short Pan from Dirty Coins" ,"https://bizweb.dktcdn.net/100/369/010/products/1-e95372e6-c524-4715-a07d-791c91ebe7ea.jpg?v=1656325763640",300000,"2023-06-30",false },
+                    {43,"US002","Nike AF1 Mid" ,"Short Pan from Dirty Coins" ,"https://cdn.vortexs.io/api/images/372b5357-2d66-4d6d-8b89-9bed5ea69bca/1920/w/50-giay-nike-air-force-1-mid-all-white-o-314195-113.jpeg",1600000,"2023-06-30",false },
+                    {43,"US004","Short TeeLab" ,"Short Local Band Unisex Special" ,"https://bizweb.dktcdn.net/thumb/large/100/415/697/products/ta9216-11k7zwk1-1-7fw9-hinh-mat-sau-0-e7d37221-9eeb-4978-868f-9193908bee74.jpg?v=1685936039000",190000,"2023-06-30",false },
+                    {43,"US003","Adidas Ultra Boots" ,"Adidas Ultra Boots Black-White" ,"https://giayxshop.vn/wp-content/uploads/2019/01/z3651678985400_f5c2a3afb17825c7244e3a4698bb798c-scaled.jpg",2500000,"2023-06-30",false },
+                    {43,"US005","Adidas Jacket" ,"" ,"https://cdn.vortexs.io/api/images/a69812de-f2c5-4243-abe0-72cf736ab4d7/1920/w/ao-khoac-jacket-adidas-firebird-track-red-hot-gf0211.jpeg",300000,"2023-06-30",false },
 
                 }
                 );
 
             migrationBuilder.InsertData(
                 table: "Comments",
-                columns: new[] { "ProductId", "UserId", "Message", "IsDeleted" },
+                columns: new[] {"ProductId", "UserId", "Message", "IsDeleted" },
                 values: new object[,]
                 {
-                    {2 , 5 , "Shoes still pretty new and fix with my foots",1 },
-                    {2 , 1 , "The pant is quite small for me ,  but still ok",1 },
-                    {5 , 2 , "Jacket look so nice , very new than the imagine",1 },
-                    {4 , 4, "Shoes still pretty new and fix with my foots",1 },
+                    {2 , "US005" , "Shoes still pretty new and fix with my foots",false },
+                    {2 , "US001" , "The pant is quite small for me ,  but still ok",false },
+                    {5 , "US002" , "Jacket look so nice , very new than the imagine",false },
+                    {4 , "US004", "Shoes still pretty new and fix with my foots",false },
                 }
                 );
 
@@ -77,15 +78,15 @@ namespace BusinessObjects.Migrations
                 columns: new[] { "ProductId", "UserId", "IsDeleted" },
                 values: new object[,]
                 {
-                    {2,1,1 },
-                    {2,3,1 },
-                    {2,5,1 },
-                    {3,2,1 },
-                    {4,2,1 },
-                    {5,3,1 },
-                    {1,2,1 },
-                    {1,4,1 },
-                    {3,1,1 }
+                    {2,"US001",false },
+                    {2,"US003",false },
+                    {2,"US005",false },
+                    {3,"US002",false },
+                    {4,"US002",false },
+                    {5,"US002",false },
+                    {1,"US003",false },
+                    {1,"US004",false },
+                    {3,"US001",false }
                 }
                 );
             migrationBuilder.InsertData(
@@ -93,8 +94,8 @@ namespace BusinessObjects.Migrations
               columns: new[] { "UserId", "ProductId", "DeliverMethod", "DeliverDetais", "PaymentMethod", "PaymentDetais", "OrderStatus", "Note", "IsDeleted" },
               values: new object[,]
               {
-                    {1,2,"Grab","Nothing" , "Grab will collect on my behalf " , "Card payment" ," On the way " ,"Nothing",false },
-                    {2,3,"Self-deliver","Nothing" , "Pay Directly" , "Card payment" ,"Preparing" ,"Nothing",false },
+                    {"US001",2,"Grab","Nothing" , "Grab will collect on my behalf " , "Card payment" ," On the way " ,"Nothing",false },
+                    {"US002",3,"Self-deliver","Nothing" , "Pay Directly" , "Card payment" ,"Preparing" ,"Nothing",false },
               }
               );
         }
