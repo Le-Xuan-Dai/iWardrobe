09d6fc76ea0f48694adcefe4a9309ed64f377bf8
Le Xuan Dai <Dyan@nexondv.com>
2023-07-04 16:48:39 +0700

    update layout

--- WebApplication/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ WebApplication/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -4,12 +4,14 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using BusinessObjects;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApplication.Areas.Identity.Pages.Account.Manage
 {
+    [Authorize]
     public partial class IndexModel : PageModel
     {
         private readonly UserManager<User> _userManager;
