287a6d0635478789fcd29f18acdc98dc9a7d6e8a
LeXuanDai0307 <lxdai0307@gmail.com>
2023-07-05 00:41:49 +0700

    IW-40 add role management

---
+++ WebApplication/Areas/Admin/Pages/Roles/RolePageModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication.Areas.Admin.Pages.Roles
+{
+    public class RolePageModel : PageModel
+    {
+        protected readonly RoleManager<IdentityRole> _roleManager;
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public RolePageModel(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+    }
+}
